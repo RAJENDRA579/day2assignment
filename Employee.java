d16ba63dde2acd95d17d99271b5bbc6fe46572f3
RAJENDRA579 <102034959+RAJENDRA579@users.noreply.github.com>
2022-04-08 12:32:45 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,97 @@
+package com.sbi.emp;
+import java.time.LocalDate;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+public class Employee {
+	
+	private int employeeNumber;
+	private String name;
+	private String job;
+	private Integer manager;
+	private LocalDate joiningDate;
+	private double salary;
+	private Integer comm;
+	private Integer deptno;
+
+	public Integer getManager() {
+		return manager;
+	}
+
+	public void setManager(Integer manager) {
+		this.manager = manager;
+	}
+
+	public Integer getComm() {
+		return comm;
+	}
+
+	public void setComm(Integer comm) {
+		this.comm = comm;
+	}
+
+	public Integer getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(Integer deptno) {
+		this.deptno = deptno;
+	}
+
+	public Employee() {
+		super();
+		System.out.println("Employee created....");
+	}
+
+	public int getEmployeeNumber() {
+		return employeeNumber;
+	}
+
+	public void setEmployeeNumber(int employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public LocalDate getJoiningDate() {
+		return joiningDate;
+	}
+
+	public void setJoiningDate(LocalDate joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+	
+}
