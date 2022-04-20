c831d15459c76749d3be8ac3691aa32b9afed6fa
RAJENDRA579 <102034959+RAJENDRA579@users.noreply.github.com>
2022-04-20 18:01:44 +0530

    Add files via upload

---
+++ start2.bat
@@ -0,0 +1,3 @@
+cd C:\Users\PC-18\Downloads\hsqldb-2.6.1\hsqldb\lib
+
+java -jar hsqldb.jar
---
+++ startdaily.bat
@@ -0,0 +1,2 @@
+cd C:\Users\PC-18\Downloads\hsqldb-2.6.1\hsqldb\lib
+java -cp hsqldb.jar org.hsqldb.server.Server --database.0 file:mydb --dbname.0 xdb
